e506d0a9b480d8ef68386b55f86c70597634b52c
onats1 <donatemowo@yahoo.com>
2020-10-18 20:00:28 +0100

    Implement Map based services.

---
+++ pet-clinic-data/src/main/java/com/onats/springpetclinic/services/maps/VetService.java
@@ -0,0 +1,33 @@
+package com.onats.springpetclinic.services.maps;
+
+import com.onats.springpetclinic.model.Vet;
+import com.onats.springpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
