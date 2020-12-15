347272a8483778a7c380a750fc686ed7741d169c
onats1 <donatemowo@yahoo.com>
2020-12-15 23:08:53 +0100

    Added specialties map

--- pet-clinic-data/src/main/java/com/onats/springpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/onats/springpetclinic/model/Specialty.java
@@ -1,6 +1,6 @@
 package com.onats.springpetclinic.model;
 
-public class Specialty extends BaseEntity{
+public class Specialty extends BaseEntity {
 
     private String description;
 
---
+++ pet-clinic-data/src/main/java/com/onats/springpetclinic/services/maps/SpecialtyServiceMap.java
@@ -0,0 +1,34 @@
+package com.onats.springpetclinic.services.maps;
+
+import com.onats.springpetclinic.model.Specialty;
+import com.onats.springpetclinic.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
